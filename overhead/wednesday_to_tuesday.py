f4af8380e277e8d6319052f9679b48e27a6294e3
Michael McEniry <mcenirm@mceniry.net>
2021-04-20 11:20:41 -0500

    showing days of week

---
+++ overhead/wednesday_to_tuesday.py
@@ -0,0 +1,15 @@
+from datetime import date, timedelta
+
+ISOWEDNESDAY = 3
+ONEDAY = timedelta(days=1)
+WEEKDAYFMT = "%a %b %d -"
+WEEKENDFMT = "%a"
+
+d = date.today()
+for i in range(7 * 3):
+    iwd = d.isoweekday()
+    if d.isoweekday() == ISOWEDNESDAY:
+        print()
+    fmt = WEEKENDFMT if iwd > 5 else WEEKDAYFMT
+    print(d.strftime(fmt))
+    d += ONEDAY
