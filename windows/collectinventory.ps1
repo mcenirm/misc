09ec72e5afe9f69b27f5ae60d55b1881f00154e3
Michael McEniry <mcenirm@mceniry.net>
2022-06-01 12:54:00 -0500

    clunky inventory collector

---
+++ windows/collectinventory.ps1
@@ -0,0 +1,277 @@
+
+$ErrorActionPreference = 'Stop'
+
+$OutputAsJson = $false
+
+$ComputerName = $env:COMPUTERNAME
+if ($ComputerName -match 'localhost') {
+    $Answer = (Read-Host -Prompt "Computer name? [$ComputerName]").Trim()
+    if ([bool] $Answer) {
+        $ComputerName = $Answer
+    }
+}
+
+$InventoryTimestamp = [datetime]::UtcNow
+$InventoryTimestampS = $InventoryTimestamp.ToString("yyyyMMdd'-'HHmmss'z'", [System.Globalization.DateTimeFormatInfo]::InvariantInfo)
+
+$WhereWhen = [ordered] @{
+    InventoryTimestampUtc = $InventoryTimestamp
+    ComputerName          = $ComputerName
+}
+$WhereWhenS = "$ComputerName $InventoryTimestampS"
+
+$ResultsDirectoryName = "Inventory $WhereWhenS"
+$Prefix = "$ResultsDirectoryName\$WhereWhenS "
+
+New-Item -ItemType Directory -Path . -Name $ResultsDirectoryName | Out-Null
+
+##########
+
+$Tag = 'LocalUser'
+$LocalUsers = Get-LocalUser | Sort-Object -Property Name
+if ($OutputAsJson) {
+    $LocalUsers | ConvertTo-Json -Depth 1 | Set-Content -Path "${Prefix}${Tag}.json"
+}
+$LocalUsers | ForEach-Object {
+    New-Object PSObject -Property ($WhereWhen + [ordered] @{
+            Name        = $_.Name
+            Description = $_.Description
+
+            # AccountExpires         = $_.AccountExpires
+            Enabled     = $_.Enabled
+            FullName    = $_.FullName
+            # LastLogon              = $_.LastLogon
+            # ObjectClass            = $_.ObjectClass
+            # PasswordChangeableDate = $_.PasswordChangeableDate
+            # PasswordExpires        = $_.PasswordExpires
+            # PasswordLastSet        = $_.PasswordLastSet
+            # PasswordRequired       = $_.PasswordRequired
+            # PrincipalSource        = $_.PrincipalSource
+            # SID                    = $_.SID
+            # UserMayChangePassword  = $_.UserMayChangePassword
+        })
+} | Export-Csv -NoTypeInformation -Path "${Prefix}${Tag}.csv"
+
+##########
+
+$LocalGroups = Get-LocalGroup | Sort-Object -Property Name
+$LocalGroupsWithMembers = $LocalGroups | ForEach-Object {
+    $m = $_ | Get-LocalGroupMember | Sort-Object -Property Name
+    if ($null -eq $m) {
+        # ConvertTo-Json writes $null as empty object "{}" instead of empty list "[]"
+        $m = @()
+    }
+    @{
+        Group   = $_
+        Members = $m
+    }
+}
+
+$Tag = 'LocalGroup'
+if ($OutputAsJson) {
+    $LocalGroups | ConvertTo-Json -Depth 1 | Set-Content -Path "${Prefix}${Tag}.json"
+}
+$LocalGroups | ForEach-Object {
+    New-Object PSObject -Property ($WhereWhen + [ordered] @{
+            Name        = $_.Name
+            Description = $_.Description
+
+            # ObjectClass     = $_.ObjectClass
+            # PrincipalSource = $_.PrincipalSource
+            # SID             = $_.SID
+        })
+} | Export-Csv -NoTypeInformation -Path "${Prefix}${Tag}.csv"
+
+$Tag = 'LocalGroupMember'
+if ($OutputAsJson) {
+    $LocalGroupsWithMembers | ConvertTo-Json -Depth 2 | Set-Content -Path "${Prefix}${Tag}.json"
+}
+$LocalGroupsWithMembers | ForEach-Object {
+    $g = $_.Group.Name
+    $_.Members | ForEach-Object {
+        New-Object PSObject -Property ($WhereWhen + [ordered] @{
+                Group                 = $g
+                Member                = $_.Name
+                MemberObjectClass     = $_.ObjectClass
+                MemberPrincipalSource = $_.PrincipalSource
+            })
+    }
+} | Export-Csv -NoTypeInformation -Path "${Prefix}${Tag}.csv"
+
+##########
+
+$Tag = 'Win32 Package'
+$Win32Packages = Get-Package -AllVersions -IncludeWindowsInstaller -IncludeSystemComponent | Sort-Object -Property Name, Version
+if ($OutputAsJson) {
+    $Win32Packages | ConvertTo-Json -Depth 1 | Set-Content -Path "${Prefix}${Tag}.json"
+}
+$Win32Packages | ForEach-Object {
+    New-Object PSObject -Property ($WhereWhen + [ordered] @{
+            Name                 = $_.Name
+            Version              = $_.Version
+            Status               = $_.Status
+            FullPath             = $_.FullPath
+            
+            # AppliesToMedia             = $_.AppliesToMedia
+            # Attributes                 = $_.Attributes
+            CanonicalId          = $_.CanonicalId
+            # Culture                    = $_.Culture
+            # Dependencies               = $_.Dependencies
+            # Entities                   = $_.Entities
+            # Evidence                   = $_.Evidence
+            FastPackageReference = $_.FastPackageReference
+            FromTrustedSource    = $_.FromTrustedSource
+            # IsCorpus                   = $_.IsCorpus
+            # IsPatch                    = $_.IsPatch
+            # IsSupplemental             = $_.IsSupplemental
+            # Links                      = $_.Links
+            # Meta                       = $_.Meta
+            # Metadata                   = $_.Metadata
+            # PackageFilename            = $_.PackageFilename
+            # Payload                    = $_.Payload
+            # PropertyOfSoftwareIdentity = $_.PropertyOfSoftwareIdentity
+            ProviderName         = $_.ProviderName
+            # SearchKey                  = $_.SearchKey
+            # Source                     = $_.Source
+            # Summary                    = $_.Summary
+            # SwidTags                   = $_.SwidTags
+            # SwidTagText                = $_.SwidTagText
+            # TagId                      = $_.TagId
+            # TagVersion                 = $_.TagVersion
+            VersionScheme        = $_.VersionScheme
+        })
+} | Export-Csv -NoTypeInformation -Path "${Prefix}${Tag}.csv"
+
+##########
+
+$Tag = 'AppxPackage AllUsers'
+$AppxPackages = @("Bundle", "Framework", "Main", "Resource", "None") | ForEach-Object {
+    Get-AppxPackage -AllUsers -PackageTypeFilter $_ | Add-Member 'PackageType' $_ -PassThru
+} | Sort-Object -Property Name, Version
+if ($OutputAsJson) {
+    $AppxPackages | ConvertTo-Json -Depth 1 | Set-Content -Path "${Prefix}${Tag}.json"
+}
+$AppxPackages | ForEach-Object {
+    New-Object PSObject -Property ($WhereWhen + [ordered] @{
+            Name                   = $_.Name
+            Version                = $_.Version
+            StatusName             = $_.Status.Tostring()
+        
+            PackageType            = $_.PackageType
+            ArchitectureName       = $_.Architecture.Tostring()
+            SignatureKindName      = $_.SignatureKind.Tostring()
+            
+            Architecture           = $_.Architecture
+            # Dependencies           = $_.Dependencies
+            InstallLocation        = $_.InstallLocation
+            IsBundle               = $_.IsBundle
+            IsDevelopmentMode      = $_.IsDevelopmentMode
+            IsFramework            = $_.IsFramework
+            IsPartiallyStaged      = $_.IsPartiallyStaged
+            IsResourcePackage      = $_.IsResourcePackage
+            NonRemovable           = $_.NonRemovable
+            PackageFamilyName      = $_.PackageFamilyName
+            PackageFullName        = $_.PackageFullName
+            # PackageUserInformation = $_.PackageUserInformation
+            Publisher              = $_.Publisher
+            PublisherId            = $_.PublisherId
+            ResourceId             = $_.ResourceId
+            SignatureKind          = $_.SignatureKind
+            Status                 = $_.Status
+
+            PackageUserInformation = $_.PackageUserInformation -join "`n"
+        })
+} | Export-Csv -NoTypeInformation -Path "${Prefix}${Tag}.csv"
+
+##########
+
+$Tag = 'AppxProvisionedPackage'
+$AppxProvisionedPackages = Get-AppxProvisionedPackage -Online | Sort-Object -Property DisplayName, Version
+if ($OutputAsJson) {
+    $AppxProvisionedPackages | ConvertTo-Json -Depth 1 | Set-Content -Path "${Prefix}${Tag}.json"
+}
+$AppxProvisionedPackages | ForEach-Object {
+    New-Object PSObject -Property ($WhereWhen + [ordered] @{
+            DisplayName      = $_.DisplayName
+            Version          = $_.Version
+
+            ArchitectureName = [Windows.System.ProcessorArchitecture].GetEnumName($_.Architecture)
+
+            Architecture     = $_.Architecture
+            # Build            = $_.Build
+            InstallLocation  = $_.InstallLocation
+            # LogLevel         = $_.LogLevel
+            # LogPath          = $_.LogPath
+            # MajorVersion     = $_.MajorVersion
+            # MinorVersion     = $_.MinorVersion
+            # Online           = $_.Online
+            PackageName      = $_.PackageName
+            # Path             = $_.Path
+            PublisherId      = $_.PublisherId
+            Regions          = $_.Regions
+            ResourceId       = $_.ResourceId
+            # RestartNeeded    = $_.RestartNeeded
+            # Revision         = $_.Revision
+            # ScratchDirectory = $_.ScratchDirectory
+            # SysDrivePath     = $_.SysDrivePath
+            # WinPath          = $_.WinPath
+        })
+} | Export-Csv -NoTypeInformation -Path "${Prefix}${Tag}.csv"
+
+##########
+
+$Tag = 'Service'
+$ExcludedProperties = @(
+    'CimInstanceProperties',
+    'CimSystemProperties',
+    'ClassGuid',
+    'ConfigFile',
+    'DataFile',
+    'DefaultDataType',
+    'DependentFiles',
+    'Description',
+    'FilePath',
+    'HelpFile',
+    'InstallDate',
+    'MonitorName',
+    'OEMUrl',
+    'PDO',
+    'ProcessID',
+    'PSComputerName',
+    'SupportedPlatform',
+    'SystemCreationClassName',
+    'SystemName'
+)
+$Services = Get-CimInstance -ClassName CIM_Service | Select-Object -Property * -ExcludeProperty $ExcludedProperties | Sort-Object -Property Name, ServiceType, CimClass
+if ($OutputAsJson) {
+    $Services | ConvertTo-Json -Depth 1 | Set-Content -Path "${Prefix}${Tag}.json"
+}
+$Services | ForEach-Object {
+    New-Object PSObject -Property ($WhereWhen + [ordered] @{
+            Name               = $_.Name
+            ServiceType        = $_.ServiceType
+            CimClass           = $_.CimClass
+
+            Caption            = $_.Caption
+            CompatID           = $_.CompatID
+            CreationClassName  = $_.CreationClassName
+            DeviceClass        = $_.DeviceClass
+            DeviceID           = $_.DeviceID
+            DeviceName         = $_.DeviceName
+            DevLoader          = $_.DevLoader
+            DriverDate         = $_.DriverDate
+            DriverName         = $_.DriverName
+            DriverProviderName = $_.DriverProviderName
+            DriverVersion      = $_.DriverVersion
+            FriendlyName       = $_.FriendlyName
+            HardWareID         = $_.HardWareID
+            InfName            = $_.InfName
+            IsSigned           = $_.IsSigned
+            Location           = $_.Location
+            Manufacturer       = $_.Manufacturer
+            Signer             = $_.Signer
+            Started            = $_.Started
+            StartMode          = $_.StartMode
+            Status             = $_.Status
+        })
+} | Export-Csv -NoTypeInformation -Path "${Prefix}${Tag}.csv"
