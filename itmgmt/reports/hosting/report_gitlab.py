209e2f77bf7e5e654c41003b78c7aecfff3b371e
Michael McEniry <mcenirm@mceniry.net>
2022-09-02 10:43:57 -0500

    wip: report gitlab

--- itmgmt/reports/hosting/report_gitlab.py
+++ itmgmt/reports/hosting/report_gitlab.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
 
-from collections import defaultdict
 from csv import DictWriter
 from dataclasses import dataclass
 from itertools import islice
 from pathlib import Path
-from sys import stdout, exit
+from sys import exit, stdout
+from typing import Generator
 
 from dataset import Database, Table, connect
 from gitlab import Gitlab
 from gitlab.base import RESTObject
 from gitlab.const import AccessLevel
 from gitlab.mixins import ListMixin
-from gitlab.v4.objects.groups import Group
-from gitlab.v4.objects.projects import Project
+from gitlab.v4.objects.groups import Group, GroupManager
+from gitlab.v4.objects.members import GroupMember, ProjectMember
+from gitlab.v4.objects.projects import Project, ProjectManager
 from icecream import ic
 from rich import inspect as ri
 
@@ -31,8 +32,135 @@ DEFAULT_GITLAB_CONFIG_FILES = [str(Path("secrets", "gitlab.cfg"))]
 @dataclass(kw_only=True, frozen=True)
 class DbHelper:
     db: Database
+    GROUP_TABLE_NAME: str = "group"
+    PROJECT_TABLE_NAME: str = "project"
 
-    def add_item_with_attributes(
+    def import_groups(self, manager: GroupManager, /) -> DbHelper:
+        for group in self._iterate_groups_or_projects(manager):
+            self.import_group(group)
+        return self
+
+    def import_projects(self, manager: ProjectManager, /) -> DbHelper:
+        for project in self._iterate_groups_or_projects(manager):
+            self.import_project(project)
+        return self
+
+    def _iterate_groups_or_projects(
+        self,
+        manager: GroupManager | ProjectManager,
+        /,
+        *,
+        min_access_level: AccessLevel = AccessLevel.REPORTER,
+    ) -> Generator[Group | Project, None, None]:
+        for group_or_project in islice(
+            manager.list(
+                iterator=True,
+                min_access_level=int(min_access_level),
+            ),
+            5,
+        ):
+            yield group_or_project
+
+    def import_group(self, group: Group, /) -> DbHelper:
+        self._import_item_with_attributes(
+            table_name=self.GROUP_TABLE_NAME,
+            item=group,
+            ignore_attrs={
+                "avatar_url",
+                "default_branch_protection",
+                "emails_disabled",
+                "ldap_access",
+                "ldap_cn",
+                "lfs_enabled",
+                "mentions_disabled",
+                "namespace",
+                "request_access_enabled",
+                "require_two_factor_authentication",
+                "share_with_group_lock",
+                "two_factor_grace_period",
+            },
+        )
+        # for member in group.members.list(iterator=True):
+        #     helper.add_direct_member()
+        #     ri(member)
+        #     exit()
+        return self
+
+    def import_project(self, project: Project, /) -> DbHelper:
+        self._import_item_with_attributes(
+            table_name=self.PROJECT_TABLE_NAME,
+            item=project,
+            ignore_attrs={
+                "_links",
+                "allow_merge_on_skipped_pipeline",
+                "analytics_access_level",
+                "auto_cancel_pending_pipelines",
+                "auto_devops_deploy_strategy",
+                "autoclose_referenced_issues",
+                "avatar_url",
+                "build_timeout",
+                "builds_access_level",
+                "can_create_merge_request_in",
+                "ci_allow_fork_pipelines_to_run_in_parent_project",
+                "ci_config_path",
+                "ci_default_git_depth",
+                "ci_forward_deployment_enabled",
+                "ci_job_token_scope_enabled",
+                "ci_opt_in_jwt",
+                "ci_separated_caches",
+                "container_expiration_policy",
+                "container_registry_access_level",
+                "container_registry_enabled",
+                "enforce_auth_checks_on_uploads",
+                "external_authorization_classification_label",
+                "forking_access_level",
+                "forks_count",
+                "import_status",
+                "issues_access_level",
+                "issues_enabled",
+                "jobs_enabled",
+                "keep_latest_artifact",
+                "lfs_enabled",
+                "merge_commit_template",
+                "merge_method",
+                "merge_requests_access_level",
+                "merge_requests_enabled",
+                "namespace",
+                "only_allow_merge_if_all_discussions_are_resolved",
+                "only_allow_merge_if_pipeline_succeeds",
+                "open_issues_count",
+                "operations_access_level",
+                "packages_enabled",
+                "pages_access_level",
+                "permissions",
+                "printing_merge_request_link_enabled",
+                "public_jobs",
+                "readme_url",
+                "remove_source_branch_after_merge",
+                "repository_access_level",
+                "request_access_enabled",
+                "requirements_access_level",
+                "requirements_enabled",
+                "resolve_outdated_diff_discussions",
+                "restrict_user_defined_variables",
+                "runner_token_expiration_interval",
+                "security_and_compliance_access_level",
+                "security_and_compliance_enabled",
+                "service_desk_enabled",
+                "shared_runners_enabled",
+                "snippets_access_level",
+                "snippets_enabled",
+                "squash_commit_template",
+                "squash_option",
+                "star_count",
+                "suggestion_commit_message",
+                "wiki_access_level",
+                "wiki_enabled",
+            },
+        )
+        return self
+
+    def _import_item_with_attributes(
         self,
         *,
         table_name: str,
@@ -92,118 +220,9 @@ def get_default_gitlab(
 def import_gitlab(gl: Gitlab, /, db_connect_url="sqlite:///:memory:") -> Database:
     db = connect(db_connect_url)
     helper = DbHelper(db=db)
-    for fn, manager in [
-        (import_gitlab_group, gl.groups),
-        (import_gitlab_project, gl.projects),
-    ]:
-        manager: ListMixin = manager
-        for item in islice(
-            manager.list(
-                iterator=True,
-                min_access_level=int(AccessLevel.REPORTER),
-            ),
-            5,
-        ):
-            fn(helper, item)
+    helper.import_groups(gl.groups)
+    helper.import_projects(gl.projects)
     return db
-
-
-def import_gitlab_group(helper: DbHelper, group: Group, /) -> DbHelper:
-    helper.add_item_with_attributes(
-        table_name="group",
-        item=group,
-        ignore_attrs={
-            "avatar_url",
-            "default_branch_protection",
-            "emails_disabled",
-            "ldap_access",
-            "ldap_cn",
-            "lfs_enabled",
-            "mentions_disabled",
-            "namespace",
-            "request_access_enabled",
-            "require_two_factor_authentication",
-            "share_with_group_lock",
-            "two_factor_grace_period",
-        },
-    )
-    return helper
-
-
-def import_gitlab_project(helper: DbHelper, project: Project, /) -> DbHelper:
-    helper.add_item_with_attributes(
-        table_name="project",
-        item=project,
-        ignore_attrs={
-            "_links",
-            "allow_merge_on_skipped_pipeline",
-            "analytics_access_level",
-            "auto_cancel_pending_pipelines",
-            "auto_devops_deploy_strategy",
-            "autoclose_referenced_issues",
-            "avatar_url",
-            "build_timeout",
-            "builds_access_level",
-            "can_create_merge_request_in",
-            "ci_allow_fork_pipelines_to_run_in_parent_project",
-            "ci_config_path",
-            "ci_default_git_depth",
-            "ci_forward_deployment_enabled",
-            "ci_job_token_scope_enabled",
-            "ci_opt_in_jwt",
-            "ci_separated_caches",
-            "container_expiration_policy",
-            "container_registry_access_level",
-            "container_registry_enabled",
-            "enforce_auth_checks_on_uploads",
-            "external_authorization_classification_label",
-            "forking_access_level",
-            "forks_count",
-            "import_status",
-            "issues_access_level",
-            "issues_enabled",
-            "jobs_enabled",
-            "keep_latest_artifact",
-            "lfs_enabled",
-            "merge_commit_template",
-            "merge_method",
-            "merge_requests_access_level",
-            "merge_requests_enabled",
-            "namespace",
-            "only_allow_merge_if_all_discussions_are_resolved",
-            "only_allow_merge_if_pipeline_succeeds",
-            "open_issues_count",
-            "operations_access_level",
-            "packages_enabled",
-            "pages_access_level",
-            "permissions",
-            "printing_merge_request_link_enabled",
-            "public_jobs",
-            "readme_url",
-            "remove_source_branch_after_merge",
-            "repository_access_level",
-            "request_access_enabled",
-            "requirements_access_level",
-            "requirements_enabled",
-            "resolve_outdated_diff_discussions",
-            "restrict_user_defined_variables",
-            "runner_token_expiration_interval",
-            "security_and_compliance_access_level",
-            "security_and_compliance_enabled",
-            "service_desk_enabled",
-            "shared_runners_enabled",
-            "snippets_access_level",
-            "snippets_enabled",
-            "squash_commit_template",
-            "squash_option",
-            "star_count",
-            "suggestion_commit_message",
-            "wiki_access_level",
-            "wiki_enabled",
-        },
-        do_not_normalize_attrs={},
-    )
-    return helper
 
 
 def report_gitlab(db: Database, /) -> None:
