e178c7cabe98600a250ac2636bd627f9d1461eca
Michael McEniry <mcenirm@mceniry.net>
2022-12-11 11:26:07 -0600

    bad bash boilerplate

---
+++ familiarities/boilerplate.bash
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# vim: shiftwidth=2 tabstop=2 expandtab
+set -euo pipefail
+[[ "${TRACE-0}" == "1" ]] && set -x
+
+Usage () { cat >&2 <<EOF
+Usage: $0 foo [bar ...]
+Migrate UID/GID
+Where:
+  foo    a foo
+  bar    some bars
+Options:
+  -q     quiet
+  -n     dry run
+EOF
+}
+
+UsageExit () { Usage ; exit 1 ; }
+
+# here=$(cd -- "$(dirname -- "$BASH_SOURCE")" && /bin/pwd)
+#  OR
+# cd -- "$(dirname -- "$BASH_SOURCE")"
+
+dry_run=false
+quiet=false
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    --) shift ; break ;;
+    -q) quiet=true ; shift ;;
+    -n) dry_run=true ; shift ;;
+    -*) UsageExit ;;
+    *) break ;;
+  esac
+done
+#if [ $# -gt 0 ]
+#then
+#  foo=$1
+#  shift
+#else
+#  UsageExit
+#fi
+#if [ $# -gt 0 ]
+#then
+#  bars=( "$@" )
+#else
+#  bars=()
+#fi
+
+...
