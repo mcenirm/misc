f6e87f7b1c96877d63fe37e0b750d2fd110a6f0d
Michael McEniry <mcenirm@mceniry.net>
2023-01-03 10:56:08 -0600

    support start date argument

--- overhead/wednesday_to_tuesday.py
+++ overhead/wednesday_to_tuesday.py
@@ -1,12 +1,42 @@
-from __future__ import print_function
-from datetime import date, timedelta
+from __future__ import annotations
+
+from datetime import date, datetime, timedelta
+from sys import argv, exit, stderr
+from typing import Iterable
 
 ISOWEDNESDAY = 3
 ONEDAY = timedelta(days=1)
 WEEKDAYFMT = "%a %b %d -"
 WEEKENDFMT = "%a"
+STARTDATESTRPTIMEFORMATS = [
+    "%Y-%m-%d",
+]
 
-d = date.today()
+
+def attempt_strptime_using_several_formats(
+    datetime_string: str,
+    formats: Iterable[str],
+) -> datetime:
+    for fmt in formats:
+        try:
+            return datetime.strptime(datetime_string, fmt)
+        except ValueError:
+            pass
+    raise ValueError(f"unable to strptime: {datetime_string!r}")
+
+
+if argv[1:]:
+    try:
+        d = attempt_strptime_using_several_formats(
+            argv[1],
+            STARTDATESTRPTIMEFORMATS,
+        ).date()
+    except ValueError as ve:
+        print("Error:", *ve.args, file=stderr)
+        exit(1)
+else:
+    d = date.today()
+
 for i in range(7 * 3):
     iwd = d.isoweekday()
     if d.isoweekday() == ISOWEDNESDAY:
