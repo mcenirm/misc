40a39ca4184066bf9ab03d02612c41a1dc84cfa4
Michael McEniry <mcenirm@mceniry.net>
2023-08-16 10:49:45 -0500

    support java 8

--- java/TryURLs.java
+++ java/TryURLs.java
@@ -1,47 +1,49 @@
 import java.io.IOException;
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.security.Principal;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class TryURLs {
-    private static Logger LOG = System.getLogger(Foo.class.getName());
-
     public static void show(String name, Object value) {
-        LOG.log(Level.INFO, "{0}:  \"{1}\"", name, value);
+        System.out.format("%-14s  %s%n", name, value);
     }
 
     public static void main(String[] args) {
-        for (var a : args) {
+        String separatorLine = "----------------------------------------";
+        for (String a : args) {
+            System.out.println(separatorLine);
             show("argument", a);
             try {
-                var u = new URL(a);
+                URL u = new URL(a);
                 show("url", u);
-                var c = u.openConnection();
+                URLConnection c = u.openConnection();
                 show("connection", c);
+                HttpURLConnection httpc;
                 if (c instanceof HttpURLConnection) {
-                    var httpc = (HttpURLConnection) c;
+                    httpc = (HttpURLConnection) c;
                     httpc.setRequestMethod("GET");
                 }
                 c.connect();
                 if (c instanceof HttpsURLConnection) {
-                    var https = (HttpsURLConnection) c;
-                    var peer = https.getPeerPrincipal();
+                    HttpsURLConnection https = (HttpsURLConnection) c;
+                    Principal peer = https.getPeerPrincipal();
                     show("peer", peer);
                 }
                 if (c instanceof HttpURLConnection) {
-                    var httpc = (HttpURLConnection) c;
+                    httpc = (HttpURLConnection) c;
                     int code = httpc.getResponseCode();
                     show("status code", code);
                 }
             } catch (MalformedURLException e) {
-                LOG.log(Level.ERROR, e);
+                show("exception", e);
             } catch (IOException e) {
-                LOG.log(Level.ERROR, e);
+                show("exception", e);
             }
         }
+        System.out.println(separatorLine);
     }
 }
