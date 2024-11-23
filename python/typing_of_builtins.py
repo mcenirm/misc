42373b89f88c56f4b6d84556f5118a926f2cac1a
Michael McEniry <mcenirm@mceniry.net>
2024-11-23 12:39:07 -0600

    tables for typing of builtins

--- python/typing_of_builtins.py
+++ python/typing_of_builtins.py
@@ -23,6 +23,16 @@ typing.SupportsRound    bool
 typing.SupportsRound    float
 typing.SupportsRound    int
 
+┌────────────────────────┬──────┬─────┬───────┬─────────┐
+│ typing.Hashable        │ bool │ int │ float │ complex │
+│ typing.SupportsAbs     │ bool │ int │ float │ complex │
+│ typing.SupportsFloat   │ bool │ int │ float │         │
+│ typing.SupportsInt     │ bool │ int │ float │         │
+│ typing.SupportsRound   │ bool │ int │ float │         │
+│ typing.SupportsIndex   │ bool │ int │       │         │
+│ typing.SupportsComplex │      │     │       │ complex │
+└────────────────────────┴──────┴─────┴───────┴─────────┘
+
 ## other
 typing.AbstractSet      frozenset
 typing.AbstractSet      set
@@ -123,20 +133,70 @@ typing.Sized            tuple
 typing.SupportsBytes    bytes
 typing.Tuple            tuple
 typing.Type             type
+
+┏━━━━━━━━━━━━━━━━━━━━━━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓
+┃                        ┃ b ┃ d ┃ l ┃ m ┃ t ┃ f ┃ s ┃ b ┃ r ┃ s ┃ t ┃ e ┃ f ┃ m ┃ r ┃ z ┃ s ┃ c ┃ p ┃ s ┃ s ┃
+┃                        ┃ y ┃ i ┃ i ┃ e ┃ u ┃ r ┃ e ┃ y ┃ a ┃ t ┃ y ┃ n ┃ i ┃ a ┃ e ┃ i ┃ t ┃ l ┃ r ┃ l ┃ u ┃
+┃                        ┃ t ┃ c ┃ s ┃ m ┃ p ┃ o ┃ t ┃ t ┃ n ┃ r ┃ p ┃ u ┃ l ┃ p ┃ v ┃ p ┃ a ┃ a ┃ o ┃ i ┃ p ┃
+┃                        ┃ e ┃ t ┃ t ┃ o ┃ l ┃ z ┃   ┃ e ┃ g ┃   ┃ e ┃ m ┃ t ┃   ┃ e ┃   ┃ t ┃ s ┃ p ┃ c ┃ e ┃
+┃                        ┃ s ┃   ┃   ┃ r ┃ e ┃ e ┃   ┃ a ┃ e ┃   ┃   ┃ e ┃ e ┃   ┃ r ┃   ┃ i ┃ s ┃ e ┃ e ┃ r ┃
+┃                        ┃   ┃   ┃   ┃ y ┃   ┃ n ┃   ┃ r ┃   ┃   ┃   ┃ r ┃ r ┃   ┃ s ┃   ┃ c ┃ m ┃ r ┃   ┃   ┃
+┃                        ┃   ┃   ┃   ┃ v ┃   ┃ s ┃   ┃ r ┃   ┃   ┃   ┃ a ┃   ┃   ┃ e ┃   ┃ m ┃ e ┃ t ┃   ┃   ┃
+┃                        ┃   ┃   ┃   ┃ i ┃   ┃ e ┃   ┃ a ┃   ┃   ┃   ┃ t ┃   ┃   ┃ d ┃   ┃ e ┃ t ┃ y ┃   ┃   ┃
+┃                        ┃   ┃   ┃   ┃ e ┃   ┃ t ┃   ┃ y ┃   ┃   ┃   ┃ e ┃   ┃   ┃   ┃   ┃ t ┃ h ┃   ┃   ┃   ┃
+┃                        ┃   ┃   ┃   ┃ w ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃ h ┃ o ┃   ┃   ┃   ┃
+┃                        ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃ o ┃ d ┃   ┃   ┃   ┃
+┃                        ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃   ┃ d ┃   ┃   ┃   ┃   ┃
+┡━━━━━━━━━━━━━━━━━━━━━━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━╇━━━┩
+│ typing.Hashable        │ X │   │   │ X │ X │ X │   │   │ X │ X │ X │ X │ X │ X │ X │ X │ X │ X │ X │ X │ X │
+│ typing.Iterable        │ X │ X │ X │ X │ X │ X │ X │ X │ X │ X │   │ X │ X │ X │ X │ X │   │   │   │   │   │
+│ typing.Collection      │ X │ X │ X │ X │ X │ X │ X │ X │ X │ X │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Container       │ X │ X │ X │ X │ X │ X │ X │ X │ X │ X │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Sized           │ X │ X │ X │ X │ X │ X │ X │ X │ X │ X │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Reversible      │ X │ X │ X │ X │ X │   │   │ X │ X │ X │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Sequence        │ X │   │ X │ X │ X │   │   │ X │ X │ X │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Iterator        │   │   │   │   │   │   │   │   │   │   │   │ X │ X │ X │ X │ X │   │   │   │   │   │
+│ typing.AbstractSet     │   │   │   │   │   │ X │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Callable        │   │   │   │   │   │   │   │   │   │   │ X │   │   │   │   │   │ X │   │   │   │   │
+│ typing.MutableSequence │   │   │ X │   │   │   │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.ContextManager  │   │   │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Dict            │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.FrozenSet       │   │   │   │   │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.List            │   │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Mapping         │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.MutableMapping  │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.MutableSet      │   │   │   │   │   │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Set             │   │   │   │   │   │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.SupportsBytes   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Tuple           │   │   │   │   │ X │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+│ typing.Type            │   │   │   │   │   │   │   │   │   │   │ X │   │   │   │   │   │   │   │   │   │   │
+└────────────────────────┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
 """
 
 import builtins
-import itertools
 import numbers
 import types
 import typing
+
+try:
+    import rich
+    import rich.console
+    import rich.table
+    import rich.text
+except ImportError:
+    rich = None
+
+
+TypeName = str
+TypesInModules = dict[TypeName, type]
+SuperSubPair = tuple[TypeName, TypeName]
 
 
 def find_types_in_modules(
     *mods: types.ModuleType,
     qualify_names=True,
-) -> dict[str, type]:
-    d = {}
+) -> TypesInModules:
+    d: TypesInModules = {}
     for mod in mods:
         for name in dir(mod):
             if name.startswith("_"):
@@ -167,54 +227,84 @@ def find_types_in_modules(
     return d
 
 
-supertypes = find_types_in_modules(typing)
-subtypes = find_types_in_modules(builtins, qualify_names=False)
+def super_sub_pairs(
+    supertypes: TypesInModules,
+    subtypes: TypesInModules,
+) -> list[SuperSubPair]:
+    pairs: list[SuperSubPair] = []
+    for supername, supertype in supertypes.items():
+        for subname, subtype in subtypes.items():
+            if supertype == subtype:
+                continue
+            try:
+                if issubclass(subtype, supertype):
+                    pairs.append((supername, subname))
+            except TypeError:
+                # ignore problems with issubclass(<a generic alias>, ...)
+                pass
+    return pairs
 
-numerics_supersubs = []
-numerics_supernames_with_subtypes = set()
-numerics_subnames_with_supertypes = set()
 
-other_supersubs = []
-other_supernames_with_subtypes = set()
-other_subnames_with_supertypes = set()
+typing_types = find_types_in_modules(typing)
+builtin_types = find_types_in_modules(builtins, qualify_names=False)
+builtin_numeric_types: TypesInModules = {
+    n: t for n, t in builtin_types.items() if issubclass(t, numbers.Number)
+}
+builtin_other_types: TypesInModules = {
+    n: t for n, t in builtin_types.items() if not issubclass(t, numbers.Number)
+}
 
-for supername, subname in itertools.product(supertypes.keys(), subtypes.keys()):
-    supertype = supertypes[supername]
-    subtype = subtypes[subname]
-    if supertype == subtype:
-        continue
-    try:
-        if issubclass(subtype, supertype):
-            if issubclass(subtype, numbers.Number):
-                numerics_supersubs.append((supername, subname))
-                numerics_supernames_with_subtypes.add(supername)
-                numerics_subnames_with_supertypes.add(subname)
-            else:
-                other_supersubs.append((supername, subname))
-                other_supernames_with_subtypes.add(supername)
-                other_subnames_with_supertypes.add(subname)
-    except TypeError:
-        # ignore problems with issubclass(<a generic alias>, ...)
-        pass
+labeled_supersubs = {
+    "numerics": super_sub_pairs(typing_types, builtin_numeric_types),
+    "other": super_sub_pairs(typing_types, builtin_other_types),
+}
 
-for label, supersubs, supernames_with_subtypes, subnames_with_supertypes in [
-    (
-        "numerics",
-        numerics_supersubs,
-        numerics_supernames_with_subtypes,
-        numerics_subnames_with_supertypes,
-    ),
-    (
-        "other",
-        other_supersubs,
-        other_supernames_with_subtypes,
-        other_subnames_with_supertypes,
-    ),
-]:
+for label, supersubs in labeled_supersubs.items():
+    supercounts = {
+        sup1: sum([1 for sup2, sub2 in supersubs if sup2 == sup1])
+        for sup1, sub1 in supersubs
+    }
+    subcounts = {
+        sub1: sum([1 for sup2, sub2 in supersubs if sub2 == sub1])
+        for sup1, sub1 in supersubs
+    }
+    supernames = sorted(supercounts, key=supercounts.__getitem__, reverse=True)
+    subnames = sorted(subcounts, key=subcounts.__getitem__, reverse=True)
+    superwidth = max(map(len, supernames))
+    subwidth = max(map(len, subnames))
+
     print("##", label)
-    superwidth = max(map(len, supernames_with_subtypes))
-    subwidth = max(map(len, subnames_with_supertypes))
     fmt = f"{{supername:{superwidth}}}  {{subname}}"
     for supername, subname in supersubs:
         print(fmt.format(supername=supername, subname=subname))
     print()
+
+    if rich:
+        console = rich.console.Console()
+        use_x = sum(map(len, subnames)) > 60
+        if use_x:
+            table = rich.table.Table()
+            table.add_column()
+            for subname in subnames:
+                table.add_column(
+                    rich.text.Text(
+                        "\n".join(subname.ljust(subwidth)), justify="center"
+                    ),
+                    justify="center",
+                )
+        else:
+            table = rich.table.Table(show_header=False)
+        for supername in supernames:
+            table.add_row(
+                supername,
+                *[
+                    (
+                        ("X" if use_x else subname)
+                        if (supername, subname) in supersubs
+                        else ""
+                    )
+                    for subname in subnames
+                ],
+            )
+        console.print(table)
+        print()
