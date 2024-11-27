41ff83f18c436a00581d014fea669687c635b69b
Michael McEniry <mcenirm@mceniry.net>
2024-11-27 07:32:54 -0600

    wip: I saw it on a cat's forehead

---
+++ arts-and-crafts/logo/sigil1.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from unwisvg import SVGCanvas, SVGCircle, SVGLine
+
+print(
+    """
+
+I want it to look something like:
+
+                *                
+               ***               
+             ** * **             
+       ******   *   ******       
+*******         *         *******
+  **           ***           **  
+    ***        ***        ***    
+       *        *        *       
+        *       *       *        
+        *       *       *        
+         *      *      *         
+         *      *      *         
+        * *     *     * *        
+       *  *    ***    *  *       
+           ***********           
+
+"""
+)
+
+raise NotImplementedError
