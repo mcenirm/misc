dcf0a225a60dcf28e8c4bf59b8515bf38ae94a21
Michael McEniry <mcenirm@mceniry.net>
2025-05-30 09:35:14 -0500

    collection helpers: variations on defaultdict

--- frustra/conlecta.py
+++ frustra/conlecta.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import typing
+
 
 class NoClobberDict[_KT, _VT](dict[_KT, _VT]):
     def __setitem__(self, key: _KT, value: _VT):
@@ -11,3 +13,20 @@ class NoClobberDict[_KT, _VT](dict[_KT, _VT]):
             return
 
         raise KeyError("key already exists", key, self[key], value)
+
+
+class defaultdict_where_factory_takes_missing_key_as_arg[_KT, _VT](dict[_KT, _VT]):
+    def __init__(self, default_factory: typing.Callable[[_KT], _VT]) -> None:
+        super().__init__()
+        self.default_factory = default_factory
+
+    def __missing__(self, key: _KT) -> _VT:
+        value = self[key] = self.default_factory(key)
+        return value
+
+
+class defaultdict_where_default_value_is_missing_key(
+    defaultdict_where_factory_takes_missing_key_as_arg
+):
+    def __init__(self, *args, **kwargs):
+        super().__init__(lambda k: k, *args, **kwargs)
