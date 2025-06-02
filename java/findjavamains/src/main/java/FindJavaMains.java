efcd58e0abb9b60a5f0f90d2519672faa44359af
Michael McEniry <mcenirm@mceniry.net>
2025-06-02 16:01:10 -0500

    find java mains in multiple starting places

--- java/findjavamains/src/main/java/FindJavaMains.java
+++ java/findjavamains/src/main/java/FindJavaMains.java
@@ -3,6 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -26,44 +27,63 @@ class FindJavaMains {
             .unmodifiableSet(new HashSet<>(Arrays.asList(MAIN_DECLARATION_VALUES)));
 
     public static void main(final java.lang.String[] args) throws IOException {
-        if (args.length > 1 || (args.length == 1 && args[0].startsWith("-"))) {
-            System.err.print("Usage: ");
-            System.err.print(FindJavaMains.class.getName());
-            System.err.println(" [start]");
-            System.err.println("Find Java source files with main-capable classes");
-            System.err.println("Where:");
-            System.err.println("  start    starting directory (current directory by default)");
-            System.exit(1);
+        final ArrayList<Path> starts = new ArrayList<>();
+        int argi = 0;
+        while (argi < args.length) {
+            if ("--".equals(args[argi])) {
+                argi++;
+                break;
+            } else if (args[argi].startsWith("-")) {
+                System.err.print("Usage: ");
+                System.err.print(FindJavaMains.class.getName());
+                System.err.println(" [start ...]");
+                System.err.println("Find Java source files with main-capable classes");
+                System.err.println("Where:");
+                System.err.println("  start    starting directory (current directory by default)");
+                System.exit(1);
+            } else {
+                starts.add(Paths.get(args[argi]));
+                argi++;
+            }
         }
+        while (argi < args.length) {
+            starts.add(Paths.get(args[argi]));
+            argi++;
+        }
+        if (starts.size() < 1) {
+            starts.add(Paths.get(""));
+        }
+
         Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
         final JavaParser parser = new JavaParser();
 
-        final Path start = Paths.get(args.length > 0 ? args[0] : "");
-        Files.find(start, Integer.MAX_VALUE, FindJavaMains::isJavaSourceFile)
-                .forEach(path -> {
-                    try {
-                        final CompilationUnit cu = parser.parse(path).getResult().get();
-                        final String relativePathAsString = start.relativize(path).toString();
-                        for (final TypeDeclaration<?> t : cu.getTypes()) {
-                            final String qualifiedName = t.getFullyQualifiedName().get();
-                            boolean hasUsableMain = false;
-                            for (final MethodDeclaration m : t.getMethodsByName("main")) {
-                                final String d = m.getDeclarationAsString(true, false, false);
-                                if (MAIN_DECLARATIONS.contains(d)) {
-                                    hasUsableMain = true;
-                                    break;
+        for (final Path start : starts) {
+            Files.find(start, Integer.MAX_VALUE, FindJavaMains::isJavaSourceFile)
+                    .forEach(path -> {
+                        try {
+                            final CompilationUnit cu = parser.parse(path).getResult().get();
+                            final String relativePathAsString = start.relativize(path).toString();
+                            for (final TypeDeclaration<?> t : cu.getTypes()) {
+                                final String qualifiedName = t.getFullyQualifiedName().get();
+                                boolean hasUsableMain = false;
+                                for (final MethodDeclaration m : t.getMethodsByName("main")) {
+                                    final String d = m.getDeclarationAsString(true, false, false);
+                                    if (MAIN_DECLARATIONS.contains(d)) {
+                                        hasUsableMain = true;
+                                        break;
+                                    }
+                                }
+                                if (hasUsableMain) {
+                                    System.out.print(relativePathAsString);
+                                    System.out.print(": ");
+                                    System.out.print(qualifiedName);
+                                    System.out.println();
                                 }
                             }
-                            if (hasUsableMain) {
-                                System.out.print(relativePathAsString);
-                                System.out.print(": ");
-                                System.out.print(qualifiedName);
-                                System.out.println();
-                            }
+                        } catch (final ParseProblemException | IOException e) {
                         }
-                    } catch (final ParseProblemException | IOException e) {
-                    }
-                });
+                    });
+        }
     }
 
     public static boolean isJavaSourceFile(final Path p, final BasicFileAttributes bfa) {
