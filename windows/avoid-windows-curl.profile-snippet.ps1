482ede4f36610903a6d11ca095e59709df9a2994
Michael McEniry <mcenirm@mceniry.net>
2026-05-12 13:15:27 -0500

    ignore problems when curl is not installed for user using winget

--- windows/avoid-windows-curl.profile-snippet.ps1
+++ windows/avoid-windows-curl.profile-snippet.ps1
@@ -3,31 +3,45 @@
 
 ### Avoid Windows curl
 function Guess-BestCurlExe {
-    $CurlsToAvoid = @(
+    $curlsToAvoid = @(
         "$($env:SystemRoot)\System32\curl.exe"
     )
 
-    # First found (eg, in PATH) is best
-    $ExistingCurls = Get-Command -Name curl -CommandType Application | Where-Object {
-        $CurlsToAvoid -notcontains $_.Source
+    $existingCurls = Get-Command -Name curl -CommandType Application | Where-Object {
+        $curlsToAvoid -notcontains $_.Source
     }
-    if ($ExistingCurls.Count -gt 0) {
-        return $ExistingCurls[0].Source
+    if ($existingCurls.Count -gt 0) {
+        return $existingCurls[0].Source
     }
 
-    # Highest version is best
-    $WinGetCurls = Get-Item -Path "$($env:LOCALAPPDATA)\Microsoft\WinGet\Packages\cURL.cURL_Microsoft.Winget.Source_8wekyb3d8bbwe\curl-*-mingw\bin\curl.exe" | Where-Object {
+    $giArgs = @{
+        ErrorAction = 'SilentlyContinue'
+        Path        = @(
+            $env:LOCALAPPDATA
+            'Microsoft'
+            'WinGet'
+            'Packages'
+            'cURL.cURL_Microsoft.Winget.Source_8wekyb3d8bbwe'
+            'curl-*-mingw'
+            'bin'
+            'curl.exe'
+        ) -join '\'
+    }
+    $winGetCurls = Get-Item @giArgs | Where-Object {
         $_.VersionInfo.ProductName -eq 'The curl executable'
     } | Sort-Object {
         $_.VersionInfo.ProductVersionRaw -as [version]
     } -Descending
-    if ($WinGetCurls.Count -gt 0) {
-        return $WinGetCurls[0].FullName
+    if ($winGetCurls.Count -gt 0) {
+        return $winGetCurls[0].FullName
     }
 
     return $null
 }
-# TODO I forget why this needed two attempts...
 if (Test-Path Alias:curl) { Remove-Item Alias:curl }
 if (Test-Path Alias:curl) { Remove-Item Alias:curl }
-Set-Alias -Name curl -Value (Guess-BestCurlExe)
+Guess-BestCurlExe | ForEach-Object {
+    if ($null -ne $_) {
+        Set-Alias -Name curl -Value $_
+    }
+}
