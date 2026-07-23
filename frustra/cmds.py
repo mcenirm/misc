52187ec34c94267be8bf7ebcedcc6140842a70f2
Michael McEniry <mcenirm@mceniry.net>
2026-07-23 11:47:08 -0500

    shebang

--- frustra/cmds.py
+++ frustra/cmds.py
@@ -1,6 +1,9 @@
 import argparse
 import collections.abc
+import dataclasses
 import inspect
+import re
+import shlex
 import typing
 
 
@@ -22,3 +25,70 @@ def argument_parser_from_function(
             kw["help"] = n.replace("_", " ")
         ap.add_argument(opt, **kw)
     return ap
+
+
+@dataclasses.dataclass
+class ParsedShebangLine:
+    line: str
+    interpreter: str
+    args: list[str]
+    env: str
+    env_opts: list[tuple[str, str]]
+    env_vars: list[tuple[str, str]]
+
+
+def parse_shebang(line: str) -> ParsedShebangLine | None:
+    if line and line.startswith("#!") and (shebang := line[2:].strip()):
+        if re.compile(r"\s*/?env\s+-S").match(shebang):
+            raise NotImplementedError(
+                line,
+                "fancy env -S string parsing not implemented yet",
+            )
+        env = ""
+        env_opts = []
+        env_vars = []
+        interpreter, *args = shlex.split(shebang)
+        if interpreter == "env" or interpreter.endswith("/env"):
+            env = interpreter
+            interpreter = ""
+            while args:
+                opt = args.pop(0)
+                match opt:
+                    case "--":
+                        if args:
+                            interpreter = args.pop(0)
+                        break
+                    case opt if opt.startswith("--"):
+                        raise NotImplementedError(
+                            line,
+                            "env long options not implemented yet",
+                        )
+                    case "-S":
+                        raise NotImplementedError(
+                            line,
+                            "env fancy -S string parsing not implemented yet",
+                        )
+                    case "-C" | "-u" | "-P" | "-a":
+                        # common:         -C, -u
+                        # macos:          -P
+                        # GNU coreutils:  -a
+                        arg = args.pop(0) if args else ""
+                        env_opts.append((opt, arg))
+                    case opt if opt.startswith("-"):
+                        env_opts.append((opt, ""))
+                    case opt if "=" in opt:
+                        n, v = opt.split("=", maxsplit=1)
+                        env_vars.append((n, v))
+                    case _:
+                        interpreter = opt
+                        break
+        return ParsedShebangLine(
+            line=line,
+            interpreter=interpreter,
+            args=args,
+            env=env,
+            env_opts=env_opts,
+            env_vars=env_vars,
+        )
+    else:
+        return None
