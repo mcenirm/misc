bd5eeda36ca0822919455d839fbda0a829f9f48c
Michael McEniry <mcenirm@mceniry.net>
2026-07-29 12:53:09 -0500

    more __doc__

--- frustra/conlecta.py
+++ frustra/conlecta.py
@@ -6,6 +6,8 @@ import typing
 
 
 class NoClobberDict[_KT, _VT](dict[_KT, _VT]):
+    """Complains if the key already exists when trying to set a value"""
+
     def __setitem__(self, key: _KT, value: _VT):
         if key not in self:
             super().__setitem__(key, value)
@@ -18,7 +20,9 @@ class NoClobberDict[_KT, _VT](dict[_KT, _VT]):
 
 
 class defaultdict_where_factory_takes_missing_key_as_arg[_KT, _VT](dict[_KT, _VT]):
-    def __init__(self, default_factory: typing.Callable[[_KT], _VT]) -> None:
+    """Acts like defaultdict but use default_factory(key) as the value"""
+
+    def __init__(self, default_factory: collections.abc.Callable[[_KT], _VT]) -> None:
         super().__init__()
         self.default_factory = default_factory
 
@@ -30,6 +34,8 @@ class defaultdict_where_factory_takes_missing_key_as_arg[_KT, _VT](dict[_KT, _VT
 class defaultdict_where_default_value_is_missing_key(
     defaultdict_where_factory_takes_missing_key_as_arg
 ):
+    """Acts like defaultdict but use key as the value"""
+
     def __init__(self, *args, **kwargs):
         super().__init__(lambda k: k, *args, **kwargs)
 
@@ -43,9 +49,10 @@ class MapAndGroupByResultException(BaseException):
 
 
 def map_and_group_by_result[_IT, _RT](
-    fn: typing.Callable[[_IT], _RT],
-    iterable: typing.Iterable[_IT],
+    fn: collections.abc.Callable[[_IT], _RT],
+    iterable: collections.abc.Iterable[_IT],
 ) -> dict[_RT, list[_IT]]:
+    """Group items by result of calling fn"""
     results: dict[_RT, list[_IT]] = {}
     for item in iterable:
         try:
@@ -140,7 +147,7 @@ def iter_over_nested(
         collections.abc.Hashable | typing.SupportsIndex | type[list]
     ] = [],
     skip_missing=True,
-) -> typing.Generator[typing.Any, None, None]:
+) -> collections.abc.Generator[typing.Any, None, None]:
     """
     Use the keys to navigate the data structure and yield the values that are found.
 
